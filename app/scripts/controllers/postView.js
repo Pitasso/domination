0f5f8399335a4cefdadce7aac190e3db636b6b74
pitasso <suchykb@gmail.com>
2015-11-20 21:51:32 -0800

    added addComment

--- app/scripts/controllers/postView.js
+++ app/scripts/controllers/postView.js
@@ -9,6 +9,12 @@ app.controller('PostViewCtrl', ['Post', '$scope', '$state', '$stateParams', '$st
     Post.getPostDetails($stateParams.id).then(function(post) {
         $scope.post = post;
         $scope.comments = $scope.post.instance.actions.comments;
+        if(post.instance.comment_id && post.instance.comment_id.instance){
+            console.log("We have some comments here")
+            $scope.post_comment = post.instance.comment_id;
+        } else {
+            console.log("No comments yet")
+        }
     })
 
 	$scope.upvotePost = function(post) {
@@ -16,16 +22,26 @@ app.controller('PostViewCtrl', ['Post', '$scope', '$state', '$stateParams', '$st
 			$scope.$apply();
 		})
 	}
-	
-	$scope.addComment = function(post, comment) {
-        var post_model = new $stamplay.Cobject("post").Model;
-        post.comment(comment);
-        post_model.fetch($scope.post.instance._id).then(function(){
-            $scope.comments = post_model.getComments();
-            $scope.new_comment = "";
-            $scope.$apply();            
+
+    $scope.addComment = function(comment, id) {
+        $scope.processing_comment = true;
+        Post.addComment(comment, id, $scope.post.instance.owner.email).then(function(res) {
+            $scope.post_comment = res;
+            $scope.comment_form = false;
+            $scope.processing_comment = false;
+            Materialize.toast("Comment has been added.", 2000)
         });
     }
+	
+	// $scope.addReply = function(post, reply) {
+ //        var post_model = new $stamplay.Cobject("post").Model;
+ //        post.comment(comment);
+ //        post_model.fetch($scope.comment.instance._id).then(function(){
+ //            $scope.comments = post_model.getComments();
+ //            $scope.new_comment = "";
+ //            $scope.$apply();            
+ //        });
+ //    }
 
 
 
