c7b35462ec58922d2816466423bb649d2a804d5a
isaiahgrey93 <isaiahgrey@gmail.com>
2015-11-21 15:42:27 -0800

    added upvoted view

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -2,21 +2,6 @@
 
 app.controller('ProfileCtrl', ['User', '$scope', '$state', "$stateParams", function(User, $scope, $state, $stateParams) {
 	var username = $stateParams.username;
-	User.getUser(username).then(function(profile) {
-		$scope.user_profile = profile;
-		var user = new Stamplay.User().Model;
-		var posts = new Stamplay.Cobject('post').Collection;
-		user.followedBy(profile.instance._id).then(function(following) {
-			console.log("Following:", following);
-			console.log("<-->");
-		})
-		posts.equalTo("owner", profile.instance._id).populateOwner().fetch().then(function() {
-			console.log(posts);
-			console.log("<-->");
-			$scope.submitted = posts.instance;
-		})
-
-	});
 
 	$scope.follow = function() {
 		console.log("attempt follow")
@@ -24,10 +9,43 @@ app.controller('ProfileCtrl', ['User', '$scope', '$state', "$stateParams", funct
 			console.log("You are now following " + username);
 		}, function(followed) {
 			console.log(followed)
-	})
+		})
+	}
 
+	var getSubmitted = function(profile) {
+		var posts = new Stamplay.Cobject('post').Collection;
+		posts.equalTo("owner", profile.instance._id).populateOwner().fetch().then(function() {
+			$scope.submitted = posts.instance;
+		})
+	}
 
-}
+	var getUpvoted = function(profile) {
+		User.getUpvoted(profile).then(function(upvoted) {
+			upvoted = upvoted.data;
+			upvoted.forEach( function(item, idx, arr) {
+				var user = new Stamplay.User().Model;
+				user.fetch(item.owner).then(function() {
+					item.owner = user.instance;
+					if(idx === arr.length - 1) {
+						$scope.upvoted = upvoted;
+						$scope.$apply();
+					}
+				})
+			})
+		})
+	}
 
+	var getFollowedBy = function(profile) {
+		var user = new Stamplay.User().Model;
+		user.followedBy(profile.instance._id).then(function(following) {
+			console.log("Following:", following);
+		})
+	}
+
+	User.getUser(username).then(function(profile) {
+		$scope.user_profile = profile;
+		getSubmitted(profile);
+		getUpvoted(profile.instance._id);
+	});
 
 }])
