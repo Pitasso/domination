e9d67224311f4f815ace324f4b0bbb8d562e5b03
isaiahgrey93 <isaiahgrey@gmail.com>
2015-11-28 21:52:54 -0800

    updated merge conflicts

--- app/scripts/controllers/membership.js
+++ app/scripts/controllers/membership.js
@@ -1,8 +1,11 @@
 'use strict';
 
 app.controller('MembershipCtrl', ['$scope','$http', '$rootScope', '$state', '$stateParams', '$stamplay', function($scope, $http, $rootScope, $state, $stateParams, $stamplay) {
-
   var delay = setTimeout(function() {
+    if(!$rootScope.currentUser.instance.prefinery_id) {
+      alert("You haven't requested membership yet.")
+      $state.go("Home");
+    }
     $http.post("http://dota.domination.cc/api/codeblock/v1/run/prefinerydetails", { id : $rootScope.currentUser.instance.prefinery_id})
     .then(function(data) {
       console.log(data)
@@ -10,7 +13,7 @@ app.controller('MembershipCtrl', ['$scope','$http', '$rootScope', '$state', '$st
       $scope.clicks = data.data.share_clicks_count;
       $scope.signups = data.data.share_signups_count;
       $scope.processed = true;
-    }, function(error) {
+    }, function(err) {
       console.log(err)
     })
   }, 1000)
