a43ec1d54739cfd83600a493fda1b3f05e00b83d
pitasso <suchykb@gmail.com>
2015-11-30 13:17:57 -0800

    finished settings page

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -2,4 +2,23 @@
 
 app.controller('SettingsCtrl', ['User', '$scope', '$state', "$stateParams", function(User, $scope, $state, $stateParams) {
 	
+	var username = $stateParams.username;
+	
+	User.getUser(username).then(function(settings) {
+		
+	});
+
+	// var updateUserSettings = function(userId) {
+	// 	var user = new Stamplay.User().Model;
+	// 	user.followedBy(userId).then(function(followedBy) {
+	// 		$scope.followedBy = followedBy.data;
+	// 		$scope.$apply();
+	// 		if(followedBy.data.length === 0) {
+	// 			$scope.followingUser = false;
+	// 		} else {
+	// 			isFollowing(followedBy.data);
+	// 		}
+	// 	})
+	// }
+
 }])
