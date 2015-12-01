cb0659b8cfad68771640444e5dad89ec6ff94327
pitasso <suchykb@gmail.com>
2015-11-30 21:22:26 -0800

    sdk update

--- app/scripts/controllers/membership.js
+++ app/scripts/controllers/membership.js
@@ -1,12 +1,8 @@
 'use strict';
 
 app.controller('MembershipCtrl', ['$scope','$http', '$rootScope', '$state', '$stateParams', '$stamplay', function($scope, $http, $rootScope, $state, $stateParams, $stamplay) {
-  
+
   var delay = setTimeout(function() {
-    if(!$rootScope.currentUser.instance.prefinery_id) {
-      alert("You haven't requested membership yet.")
-      $state.go("Home");
-    }
     $http.post("http://dota.domination.cc/api/codeblock/v1/run/prefinerydetails", { id : $rootScope.currentUser.instance.prefinery_id})
     .then(function(data) {
       console.log(data)
